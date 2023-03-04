11a3cdc6d3f07bdeaacad55caeaba163f0527c5c
SharkNeko <>
2023-03-04 19:46:54 +0800

    fix bug

--- test_chat/index.js
+++ test_chat/index.js
@@ -1,6 +1,19 @@
 import tunnel from 'tunnel'
 import fetch from 'node-fetch'
+import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const wb = '一直对捏人没什么所谓但是卧龙的捏人是不是很不戳 能不能捏个刘亦菲 ​​​​​​'
+
 async function main() {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const promptList = fs.readFileSync(path.resolve(__dirname, 'prompt.txt'), 'utf-8').split('\n')
+  const prompt = promptList[Math.floor(Math.random() * promptList.length)].replace('${wb}', wb)
   const content = {
     model: 'gpt-3.5-turbo',
     messages: [{
@@ -8,7 +21,7 @@ async function main() {
       content: 'You are a fan of a vtuber, her name is 海海'
     }, {
       role: 'user',
-      content: `海海发了条微博，内容是：“我感觉除了哑 总是唱着唱着鼻子就堵了 之前录音也是”。请使用中文，不需要包含称呼，用一个极其弱智的语气，用反问句回复这个微博。注意，你的回复只应该包含回复内容，并且不能用双引号括起来，必须在20字以内。`
+      content: prompt
     }]
   }
   const resp = await fetch('https://api.openai.com/v1/chat/completions', {
@@ -26,7 +39,7 @@ async function main() {
     })
   })
   const data = await resp.json()
-  console.log('data', data.choices[0].message.content)
+  console.log('data', data.choices[0].message.content.replaceAll('\n', '').replaceAll(' ', ''))
 }
 
 main()
