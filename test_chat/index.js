37954f70abef53c7b1527575024b1b01e830c35d
SharkNeko <>
2023-03-07 16:56:11 +0800

    fix

--- test_chat/index.js
+++ test_chat/index.js
@@ -4,7 +4,7 @@ import fs from 'fs'
 import path from 'path'
 import { fileURLToPath } from 'url'
 
-const wb = '睡！'
+const wb = '今天运动了！4min'
 
 async function main() {
   const __filename = fileURLToPath(import.meta.url);
@@ -14,13 +14,12 @@ async function main() {
   .map(line => line.trim())
   .filter((line) => line)
   .filter(line => !line.startsWith('#'))
-  console.log(promptList)
 
   const content = {
     model: 'gpt-3.5-turbo',
     messages: [{
       role: 'system',
-      content: '假设海海是你最喜欢的一名女性主播。她发了条微博，请用粉丝的语气回复这条微博，回复不应该包含称呼，必须在10字以内。'
+      content: `假设海海是你最喜欢的一名女性主播。她发了条微博。请用友善的语气，使用数个疑问句回复这条微博，回复不应该包含称呼，必须在${process.argv[3] || 15}字以内。`
     }, {
       role: 'user',
       content: wb
@@ -35,8 +34,12 @@ async function main() {
     body: JSON.stringify(content),
   })
   const data = await resp.json()
-  const reply = data.choices[0].message.content.replaceAll('\n', '').replaceAll(' ', '')
-  console.log(reply.length, reply)
+  try {
+      const reply = data.choices[0].message.content.replaceAll('\n', '').replaceAll(' ', '')
+      console.log(reply.length, reply)
+  } catch (e) {
+    console.log(data)
+  }
 }
 
 main()
